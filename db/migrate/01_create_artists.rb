9534240458b7e77b80bb058c7546ad07241e9e45
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 00:35:17 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,7 +3,7 @@ class CreateArtists < ActiveRecord::Migration[4.2]
   def change
     create_table artists do |t|
       t.string :name
-    end 
-  end 
-  
+    end
+  end
+
 end
--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,6 +1,10 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
 
-  def change
-    create_table artists do |t|
-      t.string :name
+  def change 
+    create_table genres do |t|
+      t.string :name 
+    end 
+  end 
+
+  
 end
