662bbe813263f1647fadf3dff450811deb794676
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 00:36:26 +0000

    automatically backed up by learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,10 +1,10 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
 
-  def change 
+  def change
     create_table genres do |t|
-      t.string :name 
-    end 
-  end 
+      t.string :name
+    end
+  end
 
-  
+
 end
